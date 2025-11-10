21e6fc0be029edf7e5c6fd4bf86819e799ef4dab
William Ledda <wl@spacelinelabs.com>
2025-11-10 19:27:34 +0100

    TDD enables refactoring

--- py-git-action/batch.py
+++ py-git-action/batch.py
@@ -1,12 +1,20 @@
 from typing import Any, Iterable, List
+# def batches(iterable: Iterable[Any], batch_size: int) -> Iterable[List[Any]]:
+#     batch = []
+#     for item in iterable:
+#         batch.append(item)
+#         if len(batch) == batch_size:
+#             yield batch
+#             batch = []
+#     if batch:
+#         yield batch
+#    return results
+
+from itertools import islice
 def batches(iterable: Iterable[Any], batch_size: int) -> Iterable[List[Any]]:
-    results = []
-    batch = []
-    for item in iterable:
-        batch.append(item)
-        if len(batch) == batch_size:
-            results.append(batch)
-            batch = []
-    if batch:
-        results.append(batch)
-    return results
+    iterator = iter(iterable)
+    while True:
+        batch = list(islice(iterator, batch_size))
+        if not batch:
+            return
+        yield batch
